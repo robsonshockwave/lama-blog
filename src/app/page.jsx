d792616b0f923ae5466333caa06cffed45105e43
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-02 20:06:58 -0300

    projeto inicial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,6 @@
 export default function Home() {
+  throw new Error('This is an error');
+
   return (
     <div>
       <h1>Hello World!</h1>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Links } from './links/Links';
+
+export const Navbar = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        <Links />
+      </div>
+    </div>
+  );
+};
