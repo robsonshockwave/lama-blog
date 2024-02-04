6c7bdf528fa954bff0e3136efdaea758f736f7d8
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-04 14:09:13 -0300

    about e contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,32 @@
+import Image from 'next/image';
+import styles from './contact.module.css';
+
+export const metadata = {
+  title: 'Contact Page',
+  description: 'Contact description',
+};
+
 const ContactPage = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>Send us a message!</p>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
     </div>
   );
 };
