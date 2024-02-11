97176115c00c9ba19fd6d9199a681d6fef297b7d
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-11 20:08:15 -0300

    server actions

---
+++ src/app/serveractiontest/page.jsx
@@ -0,0 +1,22 @@
+import { addPost } from '@/lib/action';
+
+const ServerActionTestPage = () => {
+  //   const actionInComponent = async () => {
+  //     'use server';
+  //     console.log('its works!');
+  //   };
+
+  return (
+    <div>
+      <form action={addPost}>
+        <input type="text" placeholder="title" name="title" />
+        <input type="text" placeholder="desc" name="desc" />
+        <input type="text" placeholder="slug" name="slug" />
+        <input type="text" placeholder="userId" name="userId" />
+        <button>Create</button>
+      </form>
+    </div>
+  );
+};
+
+export default ServerActionTestPage;
---
+++ src/lib/action.js
@@ -0,0 +1,33 @@
+import { revalidatePath } from 'next/cache';
+import { Post } from './models';
+import { connectToDb } from './utils';
+
+export const addPost = async (formData) => {
+  'use server';
+
+  //   const title = formData.get('title');
+  //   const desc = formData.get('desc');
+  //   const slug = formData.get('slug');
+  //   const userId = formData.get('userId');
+
+  const { title, desc, slug, userId } = Object.fromEntries(formData);
+
+  try {
+    connectToDb();
+    const newPost = new Post({
+      title,
+      desc,
+      slug,
+      userId,
+    });
+
+    await newPost.save();
+    console.log('Post created successfully!');
+    revalidatePath('/blog');
+  } catch (error) {
+    console.log(error);
+    return { error: 'Something went wrong!' };
+  }
+
+  console.log(title, desc, slug, userId);
+};
